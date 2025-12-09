5b283d3972ad826e9db893c24b4086f594ddd2d6
Abeer Youssef Mohamed <abeer.youssef@gmail.com>
2025-12-10 00:26:31 +0200

    Update config.py

--- classification/config.py
+++ classification/config.py
@@ -1,16 +1,17 @@
 import concepts
 
-example_name = {'yelp_polarity': 'text', 'hotelreview': 'text'}
-concepts_from_labels = { 'yelp_polarity': ["negative","positive"], 'hotelreview': ["negative","positive"]}
-class_num = {'yelp_polarity': 2, 'hotelreview': 2}
+example_name = {'yelp_polarity': 'text', 'hotel_review': 'text'}
+concepts_from_labels = { 'yelp_polarity': ["negative","positive"], 'hotel_review': ["negative","positive"]}
+class_num = {'yelp_polarity': 2, 'hotel_review': 2}
 
 # Config for Roberta-Base baseline
-finetune_epoch = {'yelp_polarity': 2, 'hotelreview': 2}
-finetune_mlp_epoch = { 'yelp_polarity': 3,  'hotelreview': 3}
+finetune_epoch = {'yelp_polarity': 2, 'hotel_review': 2}
+finetune_mlp_epoch = { 'yelp_polarity': 3,  'hotel_review': 3}
 
 # Config for CBM training
-concept_set = {'yelp_polarity': concepts.yelpp, 'hotelreview': concepts.hotelreview}
-#cbl_epochs = { 'yelp_polarity': 3, 'hotelreview': 3}
+concept_set = {'yelp_polarity': concepts.yelpp, 'hotel_review': concepts.hotelreview}
+#cbl_epochs = { 'yelp_polarity': 3, 'hotel_review': 3}
 
-cbl_epochs = {'yelp_polarity': 2, 'hotelreview': 2}
+cbl_epochs = {'yelp_polarity': 2, 'hotel_review': 2}
+
 
